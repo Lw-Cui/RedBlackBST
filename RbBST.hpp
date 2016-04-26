90c9251e8a76969ddedb285a7f4789c93605a552
lw <cui@hellolw.com>
2016-04-26 14:47:48 +0800

    finish insert and reblance op

--- RbBST.hpp
+++ RbBST.hpp
@@ -4,17 +4,84 @@ _Pragma("once")
 struct Node;
 using sptr = std::shared_ptr<Node>;
 using wptr = std::weak_ptr<Node>;
+enum class ctype {RED, BLACK};
 struct Node {
 public:
+	Node(int d, wptr p, sptr l, sptr r, ctype c = ctype::RED)
+		:data{d}, parent{p}, left{l}, right{r}, color{c} {}
+	Node(ctype c):color{c} {}
 	int data;
-	enum class ColorType {RED, BLACK} color;
-	sptr left, right;
 	wptr parent;
+	sptr left, right;
+	ctype color;
 };
 
 class RedBlackBST {
 public:
+	RedBlackBST(): nil{new Node{ctype::BLACK}} {
+		root = nil;
+	}
+	void insert(int d) {
+		sptr y{nil}, x{root};
+		while (x != nil) {
+			y = x;
+			if (d < x->data) x = x->left;
+			else x = x->right;
+		}
+		sptr z{new Node{d, y, nil, nil}};
+		if (y == nil) root = z;
+		else if (z->data < y->data) y->left = z;
+		else y->right = z;
+		insert_fixup(z);
+	}
+
 private:
+	void insert_fixup(sptr z) {
+		sptr zp{z->parent.lock()}; 
+		while (zp->color == ctype::RED) {
+			zp = z->parent.lock();
+			sptr zpp{zp->parent.lock()};
+			sptr uncle{zpp->right};
+			if (zp == zpp->left) {
+				// Case 1:the uncle node is red
+				if (uncle->color == ctype::RED) {
+					uncle->color = zp->color = ctype::BLACK;
+					zpp->color = ctype::RED;
+					z = zpp;
+				} else {
+				// Case 2/3:the uncle node of z is black
+					if (z == zp->right) {
+					// Case 2: ... and z is rightson
+						z = zp; left_rotate(z);
+					}
+					// Case 3: ... and z is leftson
+					zp->color = ctype::BLACK;
+					zpp->color = ctype::RED;
+					right_rotate(zpp);
+				}
+			} else {
+			// same as clause with "right" and "left" exchanged
+				// Case 1:the uncle node is red
+				if (uncle->color == ctype::RED) {
+					uncle->color = zp->color = ctype::BLACK;
+					zpp->color = ctype::RED;
+					z = zpp;
+				} else {
+				// Case 2/3:the uncle node of z is black
+					if (z == zp->left) {
+					// Case 2: ... and z is rightson
+						z = zp; right_rotate(z);
+					}
+					// Case 3: ... and z is leftson
+					zp->color = ctype::BLACK;
+					zpp->color = ctype::RED;
+					left_rotate(zpp);
+				}
+			}
+		}
+		root->color = ctype::BLACK;
+	}
+
 	void right_rotate(sptr x) {
 		sptr y{x->left};
 		// turn y's right subtree into x's left subtree
