9b953a58e277f69dfb9c483e82437ec65b640af1
lw <cui@hellolw.com>
2016-05-08 22:03:51 +0800

    fix weak_ptr bug

--- RbBST.hpp
+++ RbBST.hpp
@@ -8,12 +8,13 @@ using wptr = std::weak_ptr<Node>;
 enum class ctype {RED, BLACK};
 struct Node {
 public:
-	Node(int d, wptr p, sptr l, sptr r, ctype c = ctype::RED)
-		:data{d}, parent{p}, left{l}, right{r}, color{c} {}
+	Node(int d, wptr _p, sptr l, sptr r, ctype c = ctype::RED)
+		:data{d}, p{_p}, left{l}, right{r}, color{c} {}
 	Node(ctype c):color{c} {}
+	sptr parent() {return p.lock();}
 	int data;
 	int count = 1;
-	wptr parent;
+	wptr p;
 	sptr left, right;
 	ctype color;
 };
@@ -21,17 +22,23 @@ public:
 
 class RedBlackBST {
 public:
+	int length = 0;
 	std::vector<int> get_data() {
 		std::vector<int> v;
-		get_data(root, v);
+		get_data(root, v, 0);
 		return std::move(v);
 	}
-	void get_data(sptr pos, std::vector<int> &v) {
-		if (pos == nil) return;
-		get_data(pos->left, v);
+	void get_data(sptr pos, std::vector<int> &v, int len) {
+		if (pos == nil) {
+			if (length == 0) length = len;
+			else if (length != len) assert(false);
+			return;
+		}
+		if (pos->color == ctype::BLACK) len++;
+		get_data(pos->left, v, len);
 		for (int i = 0; i < pos->count; i++)
 			v.push_back(pos->data);
-		get_data(pos->right, v);
+		get_data(pos->right, v, len);
 	}
 	RedBlackBST(): nil{new Node{ctype::BLACK}} {
 		root = nil;
@@ -48,7 +55,7 @@ public:
 				x->count++; return;
 			}
 		}
-		sptr z{new Node{d, y, nil, nil}};
+		sptr z{new Node{d, y, nil, nil, ctype::RED}};
 		if (y == nil) root = z;
 		else if (z->data < y->data) y->left = z;
 		else y->right = z;
@@ -63,7 +70,7 @@ public:
 			else if (k > x->data) x = x->right;
 			else break;
 		}
-		if (x != nil) del(y);
+		if (x != nil && !(--y->count)) del(y);
 	}
 
 	void del(sptr z) {
@@ -87,16 +94,15 @@ public:
 		// So the breaker has two color (second_color and its original color)
 			second_color = successor->color;
 			breaker = successor->right;
-			sptr parent{successor->parent.lock()};
-			if (parent != z) {
+			if (successor->parent() != z) {
 				// if successor isn't adjacent to z
 				transplant(successor, successor->right);
 				successor->right = z->right;
-				successor->right->parent = successor;
+				successor->right->p = successor;
 			}
 			transplant(z, successor);
 			successor->left = z->left;
-			successor->left->parent = successor;
+			successor->left->p = successor;
 			successor->color = z->color;
 		}
 		if (second_color == ctype::BLACK)
@@ -111,23 +117,22 @@ private:
 		while (x != root && x->color == ctype::BLACK) {
 		// If x->color == RED then it means x is a red-black node.
 		// We could break the loop and set it to black
-			sptr parent; sptr brother;
-			if (x == parent->left) {
+			sptr brother;
+			if (x == x->parent()->left) {
 			// x is leftson
-				parent = x->parent.lock();
-				brother = parent->right;
+				brother = x->parent()->right;
 				if (brother->color == ctype::RED) {
 				// Case 1: x's brother is red. It could transfer to Case 2/3/4.
 					brother->color = ctype::BLACK;
-					parent->color = ctype::RED;
-					left_rotate(parent);
-					brother = parent->right;
+					x->parent()->color = ctype::RED;
+					left_rotate(x->parent());
+					brother = x->parent()->right;
 				}
 				if (brother->left->color == ctype::BLACK 
 					&& brother->right->color == ctype::BLACK) {
 				// Case 2: black brother and black brotherson.
 					brother->color = ctype::RED;
-					x = parent;
+					x = x->parent();
 				} else {
 				// Case 3/4: black brother and one or two red brotherson.
 					if (brother->right->color == ctype::BLACK) {
@@ -137,28 +142,28 @@ private:
 						right_rotate(brother);
 					}
 					// Case 4: black brother and red left brotherson.
-					brother->color = parent->color;
-					parent->color = ctype::BLACK;
+					brother = x->parent()->right;
+					brother->color = x->parent()->color;
+					x->parent()->color = ctype::BLACK;
 					brother->right->color = ctype::BLACK;
-					left_rotate(parent);
+					left_rotate(x->parent());
 					x = root;
 				}
 			} else {
 			// x is leftson, same as above clause.
-				parent = x->parent.lock();
-				brother = parent->left;
+				brother = x->parent()->left;
 				if (brother->color == ctype::RED) {
 				// Case 1: x's brother is red. It could transfer to Case 2/3/4.
 					brother->color = ctype::BLACK;
-					parent->color = ctype::RED;
-					right_rotate(parent);
-					brother = parent->left;
+					x->parent()->color = ctype::RED;
+					right_rotate(x->parent());
+					brother = x->parent()->left;
 				}
 				if (brother->left->color == ctype::BLACK 
 					&& brother->right->color == ctype::BLACK) {
 				// Case 2: black brother and black brotherson.
 					brother->color = ctype::RED;
-					x = parent;
+					x = x->parent();
 				} else {
 				// Case 3/4: black brother and one or two red son.
 					if (brother->left->color == ctype::BLACK) {
@@ -168,10 +173,11 @@ private:
 						left_rotate(brother);
 					}
 					// Case 4: black brother and red left brotherson.
-					brother->color = parent->color;
-					parent->color = ctype::BLACK;
+					brother = x->parent()->left;
+					brother->color = x->parent()->color;
+					x->parent()->color = ctype::BLACK;
 					brother->left->color = ctype::BLACK;
-					right_rotate(parent);
+					right_rotate(x->parent());
 					x = root;
 				}
 			}
@@ -186,53 +192,50 @@ private:
 
 	// use subtree v to replace subtree u
 	void transplant(sptr u, sptr v) {
-		sptr parent{u->parent.lock()};
-		if (parent == nil) root = v;
-		else if (u == parent->left) parent->left = v;
-		else parent->right = v;
-		v->parent = parent;
+		if (u->parent() == nil) root = v;
+		else if (u == u->parent()->left) u->parent()->left = v;
+		else u->parent()->right = v;
+		v->p = u->parent();
 	}
 
 	void insert_fixup(sptr z) {
-		sptr zp{z->parent.lock()}; 
-		while (zp->color == ctype::RED) {
-			zp = z->parent.lock();
-			sptr zpp{zp->parent.lock()};
-			sptr uncle{zpp->right};
-			if (zp == zpp->left) {
+		while (z->parent()->color == ctype::RED) {
+			if (z->parent() == z->parent()->parent()->left) {
 				// Case 1:the uncle node is red
+				sptr uncle{z->parent()->parent()->right};
 				if (uncle->color == ctype::RED) {
-					uncle->color = zp->color = ctype::BLACK;
-					zpp->color = ctype::RED;
-					z = zpp;
+					uncle->color = z->parent()->color = ctype::BLACK;
+					z->parent()->parent()->color = ctype::RED;
+					z = z->parent()->parent();
 				} else {
 				// Case 2/3:the uncle node of z is black
-					if (z == zp->right) {
+					if (z == z->parent()->right) {
 					// Case 2: ... and z is rightson
-						z = zp; left_rotate(z);
+						z = z->parent(); left_rotate(z);
 					}
 					// Case 3: ... and z is leftson
-					zp->color = ctype::BLACK;
-					zpp->color = ctype::RED;
-					right_rotate(zpp);
+					z->parent()->color = ctype::BLACK;
+					z->parent()->parent()->color = ctype::RED;
+					right_rotate(z->parent()->parent());
 				}
 			} else {
 			// same as clause with "right" and "left" exchanged
 				// Case 1:the uncle node is red
+				sptr uncle{z->parent()->parent()->left};
 				if (uncle->color == ctype::RED) {
-					uncle->color = zp->color = ctype::BLACK;
-					zpp->color = ctype::RED;
-					z = zpp;
+					uncle->color = z->parent()->color = ctype::BLACK;
+					z->parent()->parent()->color = ctype::RED;
+					z = z->parent()->parent();
 				} else {
 				// Case 2/3:the uncle node of z is black
-					if (z == zp->left) {
+					if (z == z->parent()->left) {
 					// Case 2: ... and z is rightson
-						z = zp; right_rotate(z);
+						z = z->parent(); right_rotate(z);
 					}
 					// Case 3: ... and z is leftson
-					zp->color = ctype::BLACK;
-					zpp->color = ctype::RED;
-					left_rotate(zpp);
+					z->parent()->color = ctype::BLACK;
+					z->parent()->parent()->color = ctype::RED;
+					left_rotate(z->parent()->parent());
 				}
 			}
 		}
@@ -244,30 +247,28 @@ private:
 		// turn y's right subtree into x's left subtree
 		x->left = y->right;
 		// TODO: Could we cancel the condition (y->right != nil)?
-		y->right->parent = x;
+		y->right->p = x;
 		// link x's parent to y
-		y->parent = x->parent;
-		sptr parent{x->parent.lock()};
-		if (parent == nil) root = y;
-		else if (x == parent->left) parent->left = y;
-		else parent->right = y;
+		y->p = x->p;
+		if (x->parent() == nil) root = y;
+		else if (x == x->parent()->left) x->parent()->left = y;
+		else x->parent()->right = y;
 		y->right = x;
-		x->parent = y;
+		x->p = y;
 	}
 
 	void left_rotate(sptr x) {
 		sptr y{x->right};
 		// turn y's left subtree into x's right subtree
 		x->right = y->left;
-		y->left->parent = x;
+		y->left->p = x;
 		// link x's parent to y
-		y->parent = x->parent;
-		sptr parent{x->parent.lock()};
-		if (parent == nil) root = y;
-		else if (x == parent->left) parent->left = y;
-		else parent->right = y;
+		y->p = x->p;
+		if (x->parent() == nil) root = y;
+		else if (x == x->parent()->left) x->parent()->left = y;
+		else x->parent()->right = y;
 		y->left = x;
-		x->parent = y;
+		x->p = y;
 	}
 	sptr root, nil;
 };
